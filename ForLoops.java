89abecf01f61831d234757511979b5dddf593505
Cameronnikjou <54414662+Cameronnikjou@users.noreply.github.com>
2019-09-18 11:12:46 -0700

    Add files via upload

---
+++ ForLoops.java
@@ -0,0 +1,19 @@
+public class ForLoops
+{
+    public static void main( String args[] ) {
+       /*for(int i=1; i<=10; i++) {
+        
+        int q= i * i * i;
+        System.out.println (i +"\t" + q);
+    }*/
+ //end ForLoops class
+ /*for(int x=99; x>=1; x = x - 2) {
+    System.out.println (x +"\t") ;
+} */
+String greeting="The quick brown fox jumped over the lazy dog";
+for(int n = greeting.length(1,42);
+System.out.println (greeting);
+}
+}
+
+
